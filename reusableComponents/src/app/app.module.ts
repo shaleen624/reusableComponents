f0f787a843b1fa595cc989f37f7086189dd57605
shaleen <shaleen624@gmail.com>
2018-06-03 23:19:50 +0530

    adding drag & drop

--- reusableComponents/src/app/app.module.ts
+++ reusableComponents/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { DomHandler } from './components/directives/domhandler';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DragulaModule } from 'ng2-dragula';
+import {DndModule} from 'ng2-dnd';
+import { SortablejsModule } from 'angular-sortablejs';
 
 import { AppComponent } from './app.component';
 import { ResizableColumnsDirective } from './components/directives/column-draggable.directive';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +16,11 @@ import { ResizableColumnsDirective } from './components/directives/column-dragga
     ResizableColumnsDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragulaModule,
+    FormsModule,
+    DndModule.forRoot(),
+    SortablejsModule.forRoot({ animation: 150 }),
   ],
   providers: [DomHandler],
   bootstrap: [AppComponent]
